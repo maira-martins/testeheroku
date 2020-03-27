e40931a7a32a99a4f6d7d5bfdd7d7c689eb647ba
maira-martins <mah-martins@live.com>
2020-03-26 21:47:45 -0300

    AC2

--- primos.py
+++ primos.py
@@ -8,15 +8,15 @@ app = Flask(__name__)
 def nao_entre_em_panico():
 
     n = 542
+    primos = ""
     for i in range(1, n+1):
         divisores = 0
         for div in range(1, i+1):
             if i % div == 0:
                 divisores += 1
         if divisores == 2:
-            return(i)
-
-
+            primos = primos + str(i) + ","
+    return(primos)
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
